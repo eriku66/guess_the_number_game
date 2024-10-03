e324ed2af185e9a3e0f86571d64ace9ab226ce22
eriku66 <eriku.hashimoto@gmail.com>
2024-10-03 23:33:39 +0900

    refactor: 💡 use fixture to mock input in tests

--- test_guess_number_game.py
+++ test_guess_number_game.py
@@ -1,3 +1,4 @@
+import contextlib
 from unittest.mock import patch
 
 import pytest
@@ -11,6 +12,26 @@ from guess_number_game import (
 )
 
 
+@pytest.fixture
+def mock_input():
+    @contextlib.contextmanager
+    def _mock_input(return_value):
+        with patch("builtins.input", return_value=return_value):
+            yield
+
+    return _mock_input
+
+
+@pytest.fixture
+def mock_inputs():
+    @contextlib.contextmanager
+    def _mock_inputs(side_effect):
+        with patch("builtins.input", side_effect=side_effect):
+            yield
+
+    return _mock_inputs
+
+
 @pytest.mark.parametrize(
     "input_value, return_value, allow_blank",
     [
@@ -20,15 +41,15 @@ from guess_number_game import (
     ],
 )
 def test_get_valid_input_success(
-    input_value: str, return_value: int, allow_blank: bool
+    mock_input, input_value: str, return_value: int, allow_blank: bool
 ):
-    with patch("builtins.input", return_value=input_value):
+    with mock_input(input_value):
         assert get_valid_input("", allow_blank) == return_value
 
 
 @pytest.mark.parametrize("input_value", ["a", ""])
-def test_get_valid_input_error(input_value: str):
-    with patch("builtins.input", return_value=input_value):
+def test_get_valid_input_error(mock_input, input_value: str):
+    with mock_input(input_value):
         with pytest.raises(ValueError):
             get_valid_input("")
 
@@ -36,14 +57,14 @@ def test_get_valid_input_error(input_value: str):
 @pytest.mark.parametrize(
     "input_values, return_value", [(["1", "2"], (1, 2)), (["1", "1"], (1, 1))]
 )
-def test_get_range_success(input_values: slice, return_value: tuple):
-    with patch("builtins.input", side_effect=input_values):
+def test_get_range_success(mock_inputs, input_values: slice, return_value: tuple):
+    with mock_inputs(input_values):
         assert get_range() == return_value
 
 
 @pytest.mark.parametrize("input_values", [(["2", "1"])])
-def test_get_range_error(input_values: slice):
-    with patch("builtins.input", side_effect=input_values):
+def test_get_range_error(mock_inputs, input_values: slice):
+    with mock_inputs(input_values):
         with pytest.raises(ValueError):
             get_range()
 
@@ -53,15 +74,15 @@ def test_get_range_error(input_values: slice):
     [("1", 1, 1, 2), ("", 2, 1, 2), ("", 1, 1, 1)],
 )
 def test_get_number_of_attempts_success(
-    input_value: str, return_value: int | None, min: int, max: int
+    mock_input, input_value: str, return_value: int | None, min: int, max: int
 ):
-    with patch("builtins.input", return_value=input_value):
+    with mock_input(input_value):
         assert get_number_of_attempts(min, max) == return_value
 
 
 @pytest.mark.parametrize("input_value", [("-1"), ("0")])
-def test_get_number_of_attempts_error(input_value: str):
-    with patch("builtins.input", return_value=input_value):
+def test_get_number_of_attempts_error(mock_input, input_value: str):
+    with mock_input(input_value):
         with pytest.raises(ValueError):
             get_number_of_attempts(1, 2)
 
@@ -70,8 +91,8 @@ def test_get_number_of_attempts_error(input_value: str):
     "random_value, result_message",
     [(2, "Correct answer!"), (3, "You've reached the limit!")],
 )
-def test_play_game(capsys, random_value: int, result_message: str):
-    with patch("builtins.input", side_effect=["1", "2"]):
+def test_play_game(capsys, mock_inputs, random_value: int, result_message: str):
+    with mock_inputs(["1", "2"]):
         with patch("random.randint", return_value=random_value):
             play_game(1, 2, 2)
     captured = capsys.readouterr()
@@ -85,8 +106,8 @@ def test_play_game(capsys, random_value: int, result_message: str):
         (["a"], 0, 1),
     ],
 )
-def test_main(input_values: slice, random_value: int, exit_code: int):
-    with patch("builtins.input", side_effect=input_values):
+def test_main(mock_inputs, input_values: slice, random_value: int, exit_code: int):
+    with mock_inputs(input_values):
         with patch("random.randint", return_value=random_value):
             with pytest.raises(SystemExit) as exc_info:
                 main()
